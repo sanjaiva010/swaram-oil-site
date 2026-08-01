e84b9bae198e060e44c6693e0482e7b9f296fddc
sanjaiva010 <sanjai.25bce7966@vitapstudent.ac.in>
2026-08-01 14:21:57 +0530

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -7,7 +7,7 @@
 // 4. Paste them below
 // ============================================
 
-const SUPABASE_URL = "PASTE_YOUR_PROJECT_URL_HERE";
+const SUPABASE_URL = "https://lrsgzxkqsomghvqxhekh.supabase.co";
 const SUPABASE_ANON_KEY = "PASTE_YOUR_ANON_PUBLIC_KEY_HERE";
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
